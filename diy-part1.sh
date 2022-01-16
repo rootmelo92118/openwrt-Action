d5ae8191314660e2c6fa8c21598cb396dde69440
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2022-01-16 18:38:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '2 aCONFIG_TEST_KERNEL?=1' include/kernel-version.mk
